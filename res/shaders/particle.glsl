e4cea9d5fe659538c970fa2abc86ac330462ee2f
samcspohn <samuel.c.spohn@gmail.com>
2020-02-24 15:10:13 -0600

    set particle render data rotation back to 2 uints(compressed) from vec4

--- res/shaders/particle.glsl
+++ res/shaders/particle.glsl
@@ -56,13 +56,13 @@ struct emitterInit{
 
 
 struct d{
-    vec4 rot;
+    // vec4 rot;
 	uint xy;
     float z;
+    uint qxy;
+    uint qzw;
 	// uint qwx;
     // uint qyz;
-    // uint qxy;
-    // uint qzw;
 	uint scale_xy;
 	uint protoID;
 	uint key_life;
@@ -106,19 +106,19 @@ void setRotation(inout d item, vec4 quat){
     // setLowBits(item.qyz,uint(quat.z * 32768 + 32768));
 
 
-    // setHighBits(item.qxy,uint(quat.x * 32768 + 32768));
-    // setLowBits(item.qxy,uint(quat.y * 32768 + 32768));
-    // setHighBits(item.qzw,uint(quat.z * 32768 + 32768));
-    // setLowBits(item.qzw,uint(quat.w * 32768 + 32768));
-    item.rot = quat;
+    setHighBits(item.qxy,uint(quat.x * 32768 + 32768));
+    setLowBits(item.qxy,uint(quat.y * 32768 + 32768));
+    setHighBits(item.qzw,uint(quat.z * 32768 + 32768));
+    setLowBits(item.qzw,uint(quat.w * 32768 + 32768));
+    // item.rot = quat;
 }
 
 vec4 getRotation(inout d item){
-    return item.rot;
-    // return normalize(vec4((float(getHighBits(item.qxy)) - 32768) / 32768,
-    //             (float(getLowBits(item.qxy)) - 32768) / 32768,
-    //             (float(getHighBits(item.qzw)) - 32768) / 32768,
-    //             (float(getLowBits(item.qzw)) - 32768) / 32768));
+    // return item.rot;
+    return normalize(vec4((float(getHighBits(item.qxy)) - 32768) / 32768,
+                (float(getLowBits(item.qxy)) - 32768) / 32768,
+                (float(getHighBits(item.qzw)) - 32768) / 32768,
+                (float(getLowBits(item.qzw)) - 32768) / 32768));
     // return normalize(vec4((float(getHighBits(item.qwx)) - 32768) / 32768,
     //         (float(getLowBits(item.qwx)) - 32768) / 32768,
     //         (float(getHighBits(item.qyz)) - 32768) / 32768,
